1f7ed7b2e575803e7cd2998494322dcf68bba8b6
Caio Vitor <164382706+viteyyy@users.noreply.github.com>
2025-09-02 14:16:39 -0300

    Create 1328A.cpp

---
+++ codeforces/1328A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t, a, b, x, y;
+
+	cin >> t;
+	for(int i = 0; i < t; i++){
+		cin >> a >> b;
+		int moves = 0;
+		while(a != b){
+			if(a < b && abs(a - b) % 2 != 0){
+				a += abs(a - b);
+				moves++;
+			} else if(a < b && abs(a - b) % 2 == 0){
+				a += (abs(a - b) - 1);
+				moves++;
+			} else if(a > b && abs(a - b) % 2 != 0){
+				a -= (abs(a - b) + 1);
+				moves++;
+			} else if(a > b && abs(a - b) % 2 == 0){
+				a -= abs(a - b);
+				moves++;
+			}
+		}
+		cout << moves << endl;
+	}
+
+	return 0;
+}
