2ea79e29a3523e5cb3e10a5793f09f50dd6efe41
Caio Vitor <164382706+viteyyy@users.noreply.github.com>
2025-09-02 14:37:26 -0300

    factorial.cpp

---
+++ neps academy/factorial.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n, fatorial = 1;
+	
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		fatorial *= n - i;
+	}
+	cout << fatorial << endl;
+	
+	return 0;
+}
